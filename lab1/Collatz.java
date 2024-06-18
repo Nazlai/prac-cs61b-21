2f87aa462e600ed5b1f7877a118fc608b853c430
Joe Ho <joe.he@redso.com.hk>
2024-06-18 12:21:19 +0800

    Revert "add nextNumber implementation to collatz"
    This reverts commit d818da49cc88353c73243760f8cf5adcaac87d1a.

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -1,35 +1,17 @@
-/**
- * Class that prints the Collatz sequence starting from a given number.
- *
- * @author Joe Ho
+/** Class that prints the Collatz sequence starting from a given number.
+ *  @author YOUR NAME HERE
  */
 public class Collatz {
 
-    /**
-     * Buggy implementation of nextNumber!
-     */
+    /** Buggy implementation of nextNumber! */
     public static int nextNumber(int n) {
-        if (n == 1) {
+        if (n  == 128) {
             return 1;
+        } else if (n == 5) {
+            return 3 * n + 1;
+        } else {
+            return n * 2;
         }
-
-        if (isEven(n)) {
-            return getNextEven(n);
-        }
-
-        return getNextOdd(n);
-    }
-
-    private static int getNextOdd(int n) {
-        return n * 3 + 1;
-    }
-
-    private static int getNextEven(int n) {
-        return n / 2;
-    }
-
-    private static boolean isEven(int n) {
-        return n % 2 == 0;
     }
 
     public static void main(String[] args) {
